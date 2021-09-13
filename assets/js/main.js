14969f8107239c28bc806d3e6f6a1e9bf216ab95
Raisoku65 <89883197+Raisoku65@users.noreply.github.com>
2021-09-13 10:58:18 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,7 @@ const app = Vue.createApp({
           q1: '3159WEH62',
         },
         stage2: {
-          q1: 'いいい',
+          q1: '2832',
           // q2: 'えええ',
           // q3: 'おおお'
         },
